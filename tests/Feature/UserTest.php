00e32f5ab1444ef348855cc3fd687806e15aefc1
Loïc Lopez <loic.lopez@epitech.eu>
2019-03-04 19:06:13 -0500

    try fix bamboo errors

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use App\Models\UserProfile;
-use Artisan;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
